398fa1c2f7ecf19aaf3ec7c91485fc680ff84c6e
Kevin <kevinsasso005@gmail.com>
2023-12-18 20:07:45 -0600

    :wrench:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListMoviesComponent } from './components/list-movies/list-movies.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'movies', pathMatch: 'full'},
+  { path: 'movies', component: ListMoviesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
